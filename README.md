4effec21e5645a6c5137ae5e219b5e2dbbd5599b
Ivo Su <ivosu@Ivos-MBP.attlocal.net>
2021-06-24 10:30:45 -0400

    feat: add changes from honeycomb

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# Honeycomb
+# Movement Task
 
-[![DOI](https://zenodo.org/badge/201963539.svg)](https://zenodo.org/badge/latestdoi/201963539) [![docs](https://img.shields.io/badge/docs-stable-blue)](https://brown-ccv.github.io/honeycomb-docs/)
 
-Honeycomb is a template for reproducible psychophysiological tasks for clinic, laboratory, and home use. It is maintained by members of the [Center for Computation and Visualization](https://ccv.brown.edu) and the [Neuromotion Lab](http://borton.engin.brown.edu/) at Brown University. To learn about installation, usage and deployment please [visit our documentation](https://brown-ccv.github.io/honeycomb-docs/).
+[![Actions Status](https://github.com/brown-ccv/task-msit/workflows/Test%2C%20Build%2C%20and%20Package/badge.svg)](https://github.com/brown-ccv/task-msit/actions)
+[![Actions Status](https://github.com/brown-ccv/task-msit/workflows/Build%20at%20home%20version%20%28Windows%29/badge.svg)](https://github.com/brown-ccv/task-msit/actions)  
 
-*Note: previously named Neuro Task Starter, some references may still refer to it as such*
+
+This repo contains the Movement task. It is a [jspsych](https://www.jspsych.org/) task built with React and Electron. This task uses the [Honeycomb](https://github.com/brown-ccv/honeycomb).
+
+## Getting Started
+
+1. Clone this repo onto your computer
+```
+git clone https://github.com/brown-ccv/task-movement.git
+```
+2. Change directory into the new folder
+```
+cd task-<TASK NAME>
+```
+3. Install the dependencies
+```
+npm install 
+```
+4. Run the task in dev mode - this should launch an electron window with the task with the inspector open to the console and will hot-reload when changes are made to the app
+```
+npm run dev
+```
+5. Check out the data - the data is saved throughout the task to the users's app directory.  This is logged at the beginning of the task wherever you ran `npm run dev`.
--- public/electron.js
+++ public/electron.js
@@ -10,7 +10,10 @@ const _ = require('lodash')
 const fs = require('fs')
 const log = require('electron-log');
 
-const USE_EVENT_MARKER = (process.env.REACT_APP_USE_EVENT_MARKER === 'true')
+// Define default environment variables
+let USE_EEG = false
+let VIDEO = false
+
 // Event Trigger
 const { eventCodes, vendorId, productId, comName } = require('./config/trigger')
 const { getPort, sendToPort } = require('event-marker')
@@ -141,13 +144,23 @@ const handleEventSend = (code) => {
   }
 }
 
+// Update env variables with buildtime values from frontend
+ipc.on('updateEnvironmentVariables', (event, args) => {
+  USE_EEG = args.USE_EEG
+  VIDEO = args.USE_CAMERA
+  if(USE_EEG) {
+    setUpPort()
+    .then(() => handleEventSend(eventCodes.test_connect))
+  }
+})
+
 // EVENT TRIGGER
 
 ipc.on('trigger', (event, args) => {
   let code = args
   if (code != undefined) {
     log.info(`Event: ${_.invert(eventCodes)[code]}, code: ${code}`)
-     if (USE_EVENT_MARKER) {
+     if (USE_EEG) {
        handleEventSend(code)
      }
   }
@@ -202,13 +215,13 @@ ipc.on('data', (event, args) => {
 
 // Save Video
 ipc.on('save_video', (event, fileName, buffer) => {
-  
-  const desktop = app.getPath('desktop')
-  const name = app.getName()
-  const today = new Date()
-  const date = today.toISOString().slice(0,10)
-  const fullPath = path.join(desktop, dataDir, studyID, participantID, date, name, fileName)
-  fs.outputFile(fullPath, buffer, err => {
+  if (VIDEO){
+    const desktop = app.getPath('desktop')
+    const name = app.getName()
+    const today = new Date()
+    const date = today.toISOString().slice(0,10)
+    const fullPath = path.join(desktop, dataDir, studyID, participantID, date, name, fileName)
+    fs.outputFile(fullPath, buffer, err => {
       if (err) {
           event.sender.send(ERROR, err.message)
       } else {
@@ -216,6 +229,8 @@ ipc.on('save_video', (event, fileName, buffer) => {
         console.log(fullPath)
       }
   })
+  }
+  
 })
 
 
